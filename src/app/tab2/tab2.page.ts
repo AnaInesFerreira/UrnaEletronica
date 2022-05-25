2d2b0fb0a4ba8f70803a2196b6be4e9a047a34de
EWERSON ALVES CORREIA <ewerson.alves@comeialabs.com.br>
2022-05-24 23:10:55 -0300

    finalização das funcionalidades da urna e refatoração completa

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -12,71 +12,73 @@ export class Tab2Page {
       electoralParty: 'MDB',
       vice: 'Evelyn Oliveira',
       numberId: 123,
+      countVotes: 0,
+      percentage: 0,
     },
     {
       name: 'Julio Macedo',
       electoralParty: 'PT',
       vice: 'Evan Marques',
       numberId: 272,
+      countVotes: 0,
+      percentage: 0,
     },
     {
       name: 'Ana Ines',
       electoralParty: 'PTS',
       vice: 'Jeff trouxa',
       numberId: 789,
+      countVotes: 0,
+      percentage: 0,
     },
     {
       name: 'Kyu Hayato',
       electoralParty: 'KHT',
       vice: 'Kyu San',
       numberId: 767,
+      countVotes: 0,
+      percentage: 0,
     },
   ];
   candidatesView = this.candidates;
-  candidate;
+  candidate = null;
   totalVotes = 0;
-  candidateOne = 0;
-  candidateTwo = 0;
-  candidateThree = 0;
-  candidateFour = 0;
-  candidateOnePct = 0;
-  candidateTwoPct = 0;
-  candidateThreePct = 0;
-  candidateFourPct = 0;
-
+  search = '';
   constructor() {}
 
   filterCandidate(value: string) {
     if (value === '' || value.length < 3) {
       this.candidatesView = this.candidates;
+      this.candidate = null;
     } else {
       this.candidatesView = this.candidates.filter(
         (item) => item.numberId === Number(value)
       );
-      this.candidate = this.candidatesView;
+      this.candidate = this.candidatesView[0];
       console.log('aqui', this.candidatesView);
     }
   }
   toCorrect() {
-    this.candidatesView = [];
-    this.candidate = [];
-    console.log('toma', this.candidate);
+    this.search = '';
   }
 
   submitElection() {
+    this.countTotalVote();
+    this.candidates[0].percentage = (this.candidates[0].countVotes * 100) / this.totalVotes;
+    this.candidates[1].percentage = (this.candidates[1].countVotes * 100) / this.totalVotes;
+    this.candidates[2].percentage = (this.candidates[2].countVotes * 100) / this.totalVotes;
+    this.candidates[3].percentage = (this.candidates[3].countVotes * 100) / this.totalVotes;
+  }
+  countTotalVote() {
     this.totalVotes++;
-    if (this.candidate[0].numberId === 123) {
-      this.candidateOne++;
-      this.candidateOnePct = (this.candidateOne * 100) / this.totalVotes;
-    } else if (this.candidate[0].numberId === 272) {
-      this.candidateTwo++;
-      this.candidateTwoPct = (this.candidateTwo * 100) / this.totalVotes;
-    } else if (this.candidate[0].numberId === 789) {
-      this.candidateThree++;
-      this.candidateThreePct = (this.candidateThree * 100) / this.totalVotes;
-    } else if (this.candidate[0].numberId === 767) {
-      this.candidateFour++;
-      this.candidateFourPct = (this.candidateFour * 100) / this.totalVotes;
+    if (this.candidate.numberId === 123) {
+      this.candidates[0].countVotes++;
+    } else if (this.candidate.numberId === 272) {
+      this.candidates[1].countVotes++;
+    } else if (this.candidate.numberId === 789) {
+      this.candidates[2].countVotes++;
+    } else if (this.candidate.numberId === 767) {
+      this.candidates[3].countVotes++;
     }
   }
 }
